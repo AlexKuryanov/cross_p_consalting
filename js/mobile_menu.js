f737104caad6f496972e263f1369527417b30719
AlexKuryanov <lexxus07@yandex.ru>
2025-08-02 18:54:58 +0200

    dev: add script for mobile menu

---
+++ js/mobile_menu.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuButton = document.querySelector(".button__open");
+  const mobileMenu = document.querySelector(".nav__menu");
+  const closeButton = document.querySelector(".button__close");
+  const menuLinks = document.querySelectorAll(".nav__link");
+
+  // Open menu
+  menuButton.addEventListener("click", function () {
+    closeButton.style.display = "block";
+    menuButton.style.display = "none";
+    mobileMenu.classList.add("open");
+  });
+
+  // Close menu
+  closeButton.addEventListener("click", function () {
+    closeButton.style.display = "none";
+    menuButton.style.display = "block";
+    mobileMenu.classList.remove("open");
+  });
+
+  // Close menu on link click
+  menuLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      closeButton.style.display = "none";
+      menuButton.style.display = "block";
+      mobileMenu.classList.remove("open");
+    });
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!mobileMenu.contains(e.target) && !menuButton.contains(e.target)) {
+      mobileMenu.classList.remove("open");
+      closeButton.style.display = "none";
+      menuButton.style.display = "block";
+    }
+  });
+});
--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+import "./mobile_menu.js";
 
 const feedbackInitials = document.querySelectorAll(".feedback__initials");
 const colors = ["#4199A1", "#A1418B", "#48A141"];
