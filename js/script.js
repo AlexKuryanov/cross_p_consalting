7dff742822079c944879be09ce248d9a6ab2b2f7
AlexKuryanov <lexxus07@yandex.ru>
2025-08-03 21:45:48 +0200

    dev: set accordion functionality

--- js/script.js
+++ js/script.js
@@ -2,9 +2,24 @@ import "./mobile_menu.js";
 import "./swiper-mob.js";
 import "./desktop-slider.js";
 
+// Initialize feedback initials with different colors
 const feedbackInitials = document.querySelectorAll(".feedback__initials");
 const colors = ["#4199A1", "#A1418B", "#48A141"];
 
 feedbackInitials.forEach((initial, index) => {
   initial.style.backgroundColor = colors[index % colors.length];
+});
+
+// Accordion functionality
+const details = document.querySelectorAll(".accordion__item");
+const summaries = document.querySelectorAll(".accordion__button");
+
+summaries.forEach((summary) => {
+  summary.addEventListener("click", (e) => {
+    details.forEach((detail) => {
+      if (detail !== e.target.closest(".accordion__item")) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
 });
