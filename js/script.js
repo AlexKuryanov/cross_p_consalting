58d07cec66e53e9931cd5bf0dc4eba9c0d2b8c12
AlexKuryanov <lexxus07@yandex.ru>
2025-08-10 12:17:34 +0200

    dev: add form script

--- js/script.js
+++ js/script.js
@@ -23,3 +23,35 @@ summaries.forEach((summary) => {
     });
   });
 });
+
+// Form submission handling
+document.querySelector(".order").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const formData = {
+    name: document.getElementById("name").value,
+    socials: document.getElementById("socials").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+  // https://script.google.com/macros/s/AKfycbwL6BtB1FmalHEzl6CaNTZ8xjseC0m79b2gG737JUHXasvO9BYuvDhlD9BMtYtcw4JFwQ/exec
+
+  fetch(
+    "https://script.google.com/macros/s/AKfycbwL6BtB1FmalHEzl6CaNTZ8xjseC0m79b2gG737JUHXasvO9BYuvDhlD9BMtYtcw4JFwQ/exec",
+    {
+      method: "POST",
+      mode: "no-cors", // Google Script без CORS ответа
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }
+  )
+    .then(() => {
+      alert("Дані надіслані!");
+      document.querySelector(".order").reset();
+    })
+    .catch(() => {
+      alert("Помилка під час відправлення.");
+    });
+});
